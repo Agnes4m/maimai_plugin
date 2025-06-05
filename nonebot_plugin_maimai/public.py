02850eef7a1b5109c166cfca1ba9ae6207e61d26
Agnes Digital <Z735803792@163.com>
2025-06-05 10:43:55 +0800

    🐛修改pydantic在v2.10以前版本报错的bug

--- nonebot_plugin_maimai/public.py
+++ nonebot_plugin_maimai/public.py
@@ -26,10 +26,23 @@ class Config(BaseModel):
         extra = "ignore"
 
 
-config = Config.parse_obj(get_driver().config)
+config = Config.model_validate(get_driver().config)
 
 
-help_msg = on_command("help", aliases={"舞萌帮助", "mai帮助"})
+@on_command("help", aliases={"舞萌帮助", "mai帮助"})
+async def help_msg(matcher: Matcher):
+    help_str = """可用命令如下：
+今日舞萌 查看今天的舞萌运势
+XXXmaimaiXXX什么 随机一首歌
+随个[dx/标准][绿黄红紫白]<难度> 随机一首指定条件的乐曲
+查歌<乐曲标题的一部分> 查询符合条件的乐曲
+[绿黄红紫白]id<歌曲编号> 查询乐曲信息或谱面信息
+<歌曲别名>是什么歌 查询乐曲别名对应的乐曲
+定数查歌 <定数>  查询定数对应的乐曲
+定数查歌 <定数下限> <定数上限>
+分数线 <难度+歌曲id> <分数线> 详情请输入"分数线 帮助"查看
+搜<手元><理论><谱面确认>"""
+    await matcher.send(help_str)
 
 
 @help_msg.handle()
