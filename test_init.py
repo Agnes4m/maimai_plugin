d69675ef328fc6f5220b9f5899658e737dccd89f
Agnes Digital <Z735803792@163.com>
2025-06-09 17:28:15 +0800

    ⭐添加测试

---
+++ test_init.py
@@ -0,0 +1,16 @@
+import nonebot
+import pytest
+from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
+
+
+@pytest.mark.asyncio
+async def test_init():
+    nonebot.init()
+
+    driver = nonebot.get_driver()
+    driver.register_adapter(ONEBOT_V11Adapter)
+
+    nonebot.load_plugin("nonebot_plugin_maimai")  # 加载重启插件
+
+    # 测试代码
+    assert True
